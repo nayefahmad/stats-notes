ca9b9bd539331a8dcc31fff281ee359328a95840
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2022-11-30 16:12:33 -0800

    Refactor: set up dataclasses and functions

--- src/2022-11-30_unexplained-variance-with-and-without-systematic-factors.py
+++ src/2022-11-30_unexplained-variance-with-and-without-systematic-factors.py
@@ -16,12 +16,16 @@
 # in the sample that is used to infer the amount of variance/chance in any test
 # statistic of interest
 
+# ## Libraries
+
 from dataclasses import dataclass
-from typing import List
+from typing import List, Union
 
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LinearRegression
+
+# ## Classes and functions
 
 
 @dataclass
@@ -34,54 +38,101 @@ class Params:
     x_sigma: float
 
 
+@dataclass
+class SimulationData:
+    x: Union[List[float], np.ndarray]
+    y: Union[List[float], np.ndarray]
+    underlying_relationship: Union[List[float], np.ndarray]
+
+
+@dataclass
+class CenteredData:
+    y_centered: Union[List[float], np.ndarray]
+    resids: Union[List[float], np.ndarray]
+
+
+def generate_dataset(params: Params, seed=0) -> SimulationData:
+    np.random.seed(seed)
+    x = np.random.normal(loc=params.x_mean, scale=params.x_sigma, size=params.num_rows)
+    underlying_relationship = params.alpha + params.beta * x
+    y = underlying_relationship + np.random.normal(
+        scale=params.sigma, size=params.num_rows
+    )
+    return SimulationData(x=x, y=y, underlying_relationship=underlying_relationship)
+
+
+def plot_data(data: SimulationData, params: Params) -> None:
+    title = f"Observed data and underlying relationship \n{params}"
+    fig, ax = plt.subplots(figsize=(10, 7))
+    ax.plot(data.x, data.y, "o", mfc="none", label="observed_data")
+    ax.plot(data.x, data.underlying_relationship, label="underlying_relationship")
+    ax.set_title(title, fontsize=8)
+    ax.set_ylim(data.y.min() - 3, data.y.max() + 10)
+    ax.set_xlim(data.x.min() - 0.5, data.x.max() + 0.5)
+    ax.legend()
+    fig.show()
+
+
+def centered_data_distributions(
+    data: SimulationData, thresh: float = 0.1
+) -> CenteredData:
+    # Unexplained variance without systematic factor
+    y_centered = data.y - data.y.mean()
+
+    # Unexplained variance with systematic factor
+    X = data.x.reshape(-1, 1)
+    model = LinearRegression()
+    model.fit(X, data.y)
+    coeffs = model.coef_
+
+    thresh = thresh
+    assert abs(coeffs[0] - p.beta[0]) < thresh
+
+    train_preds = model.predict(X)
+    resids = data.y - train_preds
+
+    return CenteredData(y_centered=y_centered, resids=resids)
+
+
+def plot_unexplained_variances(data: CenteredData, params: Params) -> None:
+    title = 'Comparing "unexplained variance" with and without systematic factor'
+    title += f"\n{params}"
+    subtitles = ["Without systematic factor", "With systematic factor"]
+    fig = plt.figure(figsize=(10, 7))
+    for index, var in enumerate([data.y_centered, data.resids]):
+        ax = plt.subplot(2, 1, index + 1)
+        ax.hist(var)
+        ax.set_xlim(-15, 15)
+        ax.set_title(subtitles[index])
+    plt.suptitle(title, fontweight="bold", fontsize=8)
+    fig.tight_layout()
+    fig.show()
+
+
+# ## Single simulation run
+
+
 p = Params(num_rows=500, alpha=2, beta=[5], sigma=0.5, x_mean=5, x_sigma=1)
-
-np.random.seed(0)
-x = np.random.normal(loc=p.x_mean, scale=p.x_sigma, size=p.num_rows)
-underlying_relationship = p.alpha + p.beta * x
-y = underlying_relationship + np.random.normal(scale=p.sigma, size=p.num_rows)
-
-title = "Observed data and underlying relationship"
-fig, ax = plt.subplots()
-ax.plot(x, y, "o", mfc="none", label="observerd_data")
-ax.plot(x, underlying_relationship, label="underlying_relationship")
-ax.set_title(title)
-ax.set_ylim(y.min() - 3, y.max() + 10)
-ax.set_xlim(x.min() - 0.5, x.max() + 0.5)
-ax.legend()
-fig.show()
+data = generate_dataset(params=p)
+plot_data(data=data, params=p)
 
 # ## Set up data for comparison
-
-# Unexplained variance without systematic factor
-
-y_centered = y - y.mean()
-
-
-# Unexplained variance with systematic factor
-
-X = x.reshape(-1, 1)
-model = LinearRegression()
-model.fit(X, y)
-coeffs = model.coef_
-
-thresh = 0.1
-assert abs(coeffs[0] - p.beta[0]) < thresh
-
-train_preds = model.predict(X)
-resids = y - train_preds
-
+centered_data = centered_data_distributions(data)
 
 # ## Comparison plots
+plot_unexplained_variances(data=centered_data, params=p)
 
-title = 'Comparing "unexplained variance" with \nand without systematic factor'
-subtitles = ["Without systematic factor", "With systematic factor"]
-fig = plt.figure()
-for index, var in enumerate([y_centered, resids]):
-    ax = plt.subplot(2, 1, index + 1)
-    ax.hist(var)
-    ax.set_xlim(-15, 15)
-    ax.set_title(subtitles[index])
-plt.suptitle(title, fontweight="bold")
-fig.tight_layout()
-fig.show()
+
+# ## Multiple runs
+
+p = Params(num_rows=500, alpha=2, beta=[5], sigma=0.5, x_mean=5, x_sigma=1)
+p2 = Params(num_rows=500, alpha=2, beta=[0], sigma=0.5, x_mean=5, x_sigma=1)
+p3 = Params(num_rows=40, alpha=2, beta=[5], sigma=0.5, x_mean=5, x_sigma=1)
+p4 = Params(num_rows=40, alpha=2, beta=[5], sigma=0.01, x_mean=5, x_sigma=0.1)
+p5 = Params(num_rows=500, alpha=2, beta=[0.2], sigma=0.5, x_mean=5, x_sigma=1)
+
+for param in [p, p2, p3, p4, p5]:
+    data = generate_dataset(params=param)
+    plot_data(data=data, params=param)
+    centered_data = centered_data_distributions(data, thresh=50)
+    plot_unexplained_variances(data=centered_data, params=param)
